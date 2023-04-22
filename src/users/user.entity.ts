a3796c72764de3e161ab9aa269521a500f33fe85
i.kłusek <J6ZkqwVUn>
2023-04-22 09:47:34 +0200

    Crud Requests

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,13 +1,33 @@
-import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm'
+import { AfterLoad, AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    email: string;
+  @Column()
+  email: string;
 
-    @Column()
-    password: string;
+  @Column()
+  password: string;
+
+  // @AfterLoad()
+  // logLoad() {
+  //   console.log('Fetch user with Id: ', this.id);
+  // }
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id ', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id ', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id ', this.id);
+  }
 }
