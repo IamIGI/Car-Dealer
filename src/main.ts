3f1a208bf96fb6535db696eba3601abb95a74fe2
i.kłusek <J6ZkqwVUn>
2023-04-23 13:37:22 +0200

    Cookie Session and Authentication(Auth guard, interceptors, custom decorators)

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(
     cookieSession({
-      keys: ['randomString_djaksjdk'],
+      keys: ['randomString_djaksjdk'], //encrypt session message
     })
   );
   app.useGlobalPipes(
