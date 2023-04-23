fe68439e9736ee4d1ed208199bbd3308375f47a8
i.kłusek <J6ZkqwVUn>
2023-04-23 13:33:27 +0200

    Cookie Session and Authentication(Auth guard, interceptors, custom decorators)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['randomString_djaksjdk'],
+    })
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, //block extra properties sended in object if not defined in dto
