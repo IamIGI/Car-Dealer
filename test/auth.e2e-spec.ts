aecb6a4e5c2c4477064d8e1b6d26d6b91a411b5b
i.kłusek <J6ZkqwVUn>
2023-04-25 13:23:28 +0200

    Added new e2e test + configure env variables

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -17,7 +17,7 @@ describe('Authentication System', () => {
 
   it('handles a signup request', () => {
     const userObject = {
-      email: 'abcd@gmail.com',
+      email: 'abcde@gmail.com',
       password: 'User123!',
     };
     return request(app.getHttpServer())
@@ -29,5 +29,19 @@ describe('Authentication System', () => {
         expect(id).toBeDefined();
         expect(email).toEqual(userObject.email);
       });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'test@gmail.com';
+    const response = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'none123' })
+      .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer()).get('/auth/whoami').set('Cookie', cookie).expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
